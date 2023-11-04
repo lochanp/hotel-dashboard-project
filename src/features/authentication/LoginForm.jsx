471b8547625d47cf83ec861e7f8d18faada6d887
lochan07 <lochan@binaryveda.com>
2023-11-04 10:27:01 +0530

    changes for new user login

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import SpinnerMini from '../../ui/SpinnerMini';
 import { useLogin } from './useLogin';
 
 function LoginForm() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('admin@mail.com');
+  const [password, setPassword] = useState('12345678');
   const { login, isLoading } = useLogin();
 
   function handleSubmit(e) {
